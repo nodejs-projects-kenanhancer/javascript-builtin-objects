84ce3e06942b208e06cc92bbf1e1bd2b05d81aa7
kenan hancer <kenanhancer@kenans-mbp-2.mynet>
2020-05-23 16:08:36 +0100

    ok

--- src/demo2/app1.js
+++ src/demo2/app1.js
@@ -1,23 +1,11 @@
 var personEntryArray = [['personId', '1'], ['firstName', 'kenan'], ['lastName', 'hancer'], ['mail', 'kh@kh.com']];
 
-var person1 = Object.fromEntries(personEntryArray);
+console.log(Object.fromEntries(personEntryArray));
 
-console.log(person1);
+console.log(Object.fromEntries(new Map(personEntryArray)));
 
+console.log(Object.fromEntries([['personId', '2'], ['firstName', 'Hakan'], ['lastName', 'celik'], ['mail', 'hc@hc.com']]));
 
-var personMap = new Map(personEntryArray);
-var person2 = Object.fromEntries(personMap);
+console.log(Object.fromEntries(new Map([['personId', '3'], ['firstName', 'gamze'], ['lastName', 'boz'], ['mail', 'gb@gb.com']])));
 
-console.log(person2);
-
-var person3 = Object.fromEntries([['personId', '2'], ['firstName', 'Hakan'], ['lastName', 'celik'], ['mail', 'hc@hc.com']]);
-
-console.log(person3);
-
-var person4 = Object.fromEntries(new Map([['personId', '3'], ['firstName', 'gamze'], ['lastName', 'boz'], ['mail', 'gb@gb.com']]));
-
-console.log(person4);
-
-var person5 = Object.fromEntries(Object.entries(person4).map(([key,value])=>[key, value + " **"]));
-
-console.log(person5);
+console.log(Object.fromEntries(personEntryArray.map(([key, value]) => [key, value + " **"])));
